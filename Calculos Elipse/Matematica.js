00fe501194af0af4464447542e85d91943e07831
YaderDavid <33787527+YaderDavid@users.noreply.github.com>
2017-11-25 11:38:38 -0600

    Calculos Matematicos
    Creacion de funciones

---
+++ Calculos Elipse/Matematica.js
@@ -0,0 +1,69 @@
+
+
+function Centros_Eventuales()
+{
+
+	for (var I = 0; I <= 23; I++)
+	{
+		for (var J = 0; J <= 10; J++) 
+		{
+			Proyeccion[0].MarcadorCollecion[I].
+		}
+		Consumidor[I]
+	}
+
+	for (var I = 0; I <= 23; I++) 
+	{
+		
+	}
+
+	for (var I = 0; I <= 23; I++) 
+	{
+		
+	}
+}
+
+function Dispersion()
+{
+
+}
+
+function Dimension_Dispersion()
+{
+
+}
+
+function Exactitud_Variables_Aleatorias()
+{
+
+}
+
+function Momento_Correlacion()
+{
+
+}
+
+function Coeficiente_Correlacion()
+{
+
+}
+
+function Angulo_Eje_Simetria()
+{
+
+}
+
+function Desviaciones_Medio_Cuadraticas()
+{
+
+}
+
+function Exactitud_Nuevo_Eje()
+{
+
+}
+
+function Radios_Elipse_Sistema()
+{
+
+}
