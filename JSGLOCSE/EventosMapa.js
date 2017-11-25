d473de9a4bf243ce4f3bd8ec051d259ee5aecb22
YaderDavid <33787527+YaderDavid@users.noreply.github.com>
2017-11-25 14:13:46 -0600

    Delimitacion del mapa y metodos para convertir posiciones
    Se anadio la opcion de delimitar el mapa mediante un rectangulo la idea es que el usuario no pueda moverse de la zona delimitada y ademas se anadieron unos metodos para convertir coordenadas a punto y visceversa

--- JSGLOCSE/EventosMapa.js
+++ JSGLOCSE/EventosMapa.js
@@ -7,6 +7,48 @@ var Mapa = null;
         Mapa = new google.maps.Map(document.getElementById('Mapa'), 
         {
           center: {lat: 12.1436131, lng: -86.2607103},
-          zoom: 8
+          zoom: 15
         });
  }
+
+
+ function DelimitarMapa()
+ {
+ 	  var Rectangulo = new google.maps.Rectangle(
+ 	  {
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.4,
+          strokeWeight: 6,
+          fillColor: '#FF0000',
+          fillOpacity: 0,
+          map: Mapa,
+          bounds: Mapa.getBounds()
+      });
+
+ 	   google.maps.event.addListener(Rectangulo, 'rightclick', function(event) 
+ 	   {
+            // Poner aqui metodo para poner el mapa
+       });
+ }
+
+ 
+
+function LatLngAPunto(latLngPunto) 
+{
+  var TopeDerecha = Mapa.getProjection().fromLatLngToPoint(Mapa.getBounds().getNorthEast());
+  var IzquierdaInferior = Mapa.getProjection().fromLatLngToPoint(Mapa.getBounds().getSouthWest());
+  var Escala = Math.pow(2, Mapa.getZoom());
+  var CoordenadaMundo = Mapa.getProjection().fromLatLngToPoint(LatLngPunto);
+  return new google.maps.Point((CoordenadaMundo.x - IzquierdaInferior.x) * Escala, (CoordenadaMundo.y - TopeDerecha.y) * Escala);
+}
+
+function PuntoALatLng(Punto) 
+{
+  var TopeDerecha = Mapa.getProjection().fromLatLngToPoint(Mapa.getBounds().getNorthEast());
+  var IzquierdaInferior = Mapa.getProjection().fromLatLngToPoint(Mapa.getBounds().getSouthWest());
+  var Escala = Math.pow(2, Mapa.getZoom());
+  var CoordenadaMundo = new google.maps.Point(Punto.x / Escala + IzquierdaInferior.x, Punto.y / Escala + TopeDerecha.y);
+  return Mapa.getProjection().fromPointToLatLng(CoordenadaMundo);
+}
+
+
--- JSGLOCSE/EventosNavegador.js
+++ JSGLOCSE/EventosNavegador.js
@@ -1,9 +1,18 @@
 // Creacion de las variables a utilizar en el sistema
 var Mapa = null;
-
+var DropDownLimitarMapa = null;
 // Evento de carga de la pagina 
 $( document ).ready(function() 
 {
 	// Inicializamos el mapa
  	InicializarMapa();
+
+ 	// Recogemos las referencias de los elementos del DOM
+ 	DropDownLimitarMapa = document.getElementById('LimitarMapa');
+
+ 	$('#LimitarMapa').click(function()
+ 	{
+ 		DelimitarMapa();	
+ 	});
+
 });
