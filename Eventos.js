a461e26207dacd0cdfab824268c9c4f5e6e87273
YaderDavid <33787527+YaderDavid@users.noreply.github.com>
2017-11-25 12:49:26 -0600

    Matematica
    Variables de Calculo

---
+++ Eventos.js
@@ -0,0 +1,5 @@
+//Creacion de objetos
+var Proyecciones = new Array();
+var ProyeccionActiva = 0;
+
+Proyecciones.push({MarcadoresCollecion:[], Contador:1,CentrosEventuales : new Array(),XDispersion:0, YDispersion:0, XYCorrelacion:0, XCentro:0, YCentro:0, XSigma: 0, YSigma:0, FhiExactitud:0, PhiExactitud:0, R:0, Angulo:0, FhiSigma2:0, PhiSigma2:0, FhiRadio:0, PhiRadio:0});
---
+++ Matematica.js
@@ -0,0 +1,66 @@
+function Centros_Eventuales(Parametro,ProyeccionActiva)
+{
+
+	for (var I = 0; I <= 23; I++)
+	{
+		for (var J = 0; J <= Proyecciones[ProyeccionActiva].MarcadorCollecion.length; J++) 
+		{
+			Proyecciones[J].MarcadorCollecion[I].
+		}
+	}
+
+	for (var I = 0; I <= 23; I++) 
+	{
+		
+	}
+
+	for (var I = 0; I <= 23; I++) 
+	{
+		
+	}
+}
+
+function Dispersion()
+{
+
+}
+
+function Dimension_Dispersion()
+{
+
+}
+
+function Exactitud_Variables_Aleatorias()
+{
+
+}
+
+function Momento_Correlacion()
+{
+
+}
+
+function Coeficiente_Correlacion()
+{
+
+}
+
+function Angulo_Eje_Simetria()
+{
+
+}
+
+function Desviaciones_Medio_Cuadraticas()
+{
+
+}
+
+function Exactitud_Nuevo_Eje()
+{
+
+}
+
+function Radios_Elipse_Sistema()
+{
+
+}
