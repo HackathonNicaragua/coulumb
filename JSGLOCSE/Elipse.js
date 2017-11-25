66f0644184c85b7da4bc81281cd5b4e5bd959125
Oscar <riveramoreira04@gmail.com>
2017-11-25 14:59:26 -0600

    Funciones Dispersion y Dimension_Dispersion
    Elaboracion de las funciones Dispersion y Dimension de la dispersion, funciones que ayudan para el calculo de la elipse

--- JSGLOCSE/Elipse.js
+++ JSGLOCSE/Elipse.js
@@ -1,10 +1,13 @@
 function Centros_Eventuales(Parametro,ProyeccionActiva)
 {
-	XTemp = 0;
-	YTemp = 0;
 
 	for (var I = 0; I <= 23; I++)
 	{
+
+		XTemp = 0;
+		YTemp = 0;
+		Suma = 0;
+
 		for (var J = 0; J <= (Proyecciones[ProyeccionActiva].MarcadorCollecion.length)-1; J++) 
 		{
 
@@ -16,22 +19,52 @@ function Centros_Eventuales(Parametro,ProyeccionActiva)
 		}
 
 		Proyecciones[ProyeccionActiva].CentrosEventuales[I].X= XTemp/Suma;  
-		
+
 		Proyecciones[ProyeccionActiva].CentrosEventuales[I].Y= YTemp/Suma;  
 	}
 
+	Proyecciones[ProyeccionActiva].XCentro = 0;
+	Proyecciones[ProyeccionActiva].YCentro = 0;
 
+	for (var I = 0; I <= 23; I++)
+	{
+		Proyecciones[ProyeccionActiva].XCentro = Proyecciones[ProyeccionActiva].XCentro + Proyecciones[ProyeccionActiva].CentrosEventuales[I].X;
+
+		Proyecciones[ProyeccionActiva].YCentro = Proyecciones[ProyeccionActiva].YCentro + Proyecciones[ProyeccionActiva].CentrosEventuales[I].Y;
+	}
+
+	//Centro de la Elipse
+	Proyecciones[ProyeccionActiva].XCentro = Proyecciones[ProyeccionActiva].XCentro / 24; 
+	Proyecciones[ProyeccionActiva].YCentro = Proyecciones[ProyeccionActiva].YCentro / 24;
+
+	Dispersion();
 	
 }
 
 function Dispersion()
 {
+	Proyecciones[ProyeccionActiva].XDispersion = 0;
+	Proyecciones[ProyeccionActiva].YDispersion = 0;
+	
+	for (var I = 0; I <= 23; I++)
+	{
+
+		Proyecciones[ProyeccionActiva].XDispersion = Proyecciones[ProyeccionActiva].XDispersion + Math.pow((Proyecciones[ProyeccionActiva].CentrosEventuales[I].X - Proyecciones[ProyeccionActiva].XCentro),2);
+
+		Proyecciones[ProyeccionActiva].YDispersion = Proyecciones[ProyeccionActiva].YDispersion + Math.pow((Proyecciones[ProyeccionActiva].CentrosEventuales[I].Y - Proyecciones[ProyeccionActiva].YCentro),2);
+	}
+
+	Proyecciones[ProyeccionActiva].XDispersion = Proyecciones[ProyeccionActiva].XDispersion / 23;
+
+	Proyecciones[ProyeccionActiva].YDispersion = Proyecciones[ProyeccionActiva].YDispersion / 23;
 
 }
 
 function Dimension_Dispersion()
 {
+	Proyecciones[ProyeccionActiva].XSigma = Math.sqrt(Proyecciones[ProyeccionActiva].XDispersion);
 
+	Proyecciones[ProyeccionActiva].YSigma = Math.sqrt(Proyecciones[ProyeccionActiva].YDispersion);
 }
 
 function Exactitud_Variables_Aleatorias()
