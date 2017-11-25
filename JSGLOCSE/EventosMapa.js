0ab3bf4957624716b4490229f8300001148a1145
YaderDavid <33787527+YaderDavid@users.noreply.github.com>
2017-11-25 17:39:31 -0600

    Finalizacion de la parte de limitar el mapa
    Ahora se puede limitar el mapa y el usuario no va poder salir de la zona delimitada

--- JSGLOCSE/EventosMapa.js
+++ JSGLOCSE/EventosMapa.js
@@ -2,28 +2,62 @@
 var Mapa = null;
 var Rectangulo = null;
 var UltimoCentro = null;
-
+var CondicionZoom =false;
+var ZoomEscucha = false;
+var ZoomBordes;
+var Bordes;
+var ZoomMinimo;
  // Inicializacion del mapa
  function InicializarMapa() 
  {
-        Mapa = new google.maps.Map(document.getElementById('Mapa'), 
-        {
-          center: {lat: 12.1436131, lng: -86.2607103},
-          zoom: 15
-        });
+   
+    Mapa = new google.maps.Map(document.getElementById('Mapa'), 
+    {
+      center: {lat: 12.1436131, lng: -86.2607103},
+      zoom: 15
+    });
 
-        
-		Mapa.addListener('bounds_changed', function() 
-		{
-			if(Rectangulo != null)
-			{
-			    if(Mapa.getBounds().contains(UltimoCentro) == false)
-			    {
-			    	Mapa.setCenter(UltimoCentro);
-			    }				 
-  
-			}
-		});
+        	
+    google.maps.event.addListener(Mapa, 'center_changed', function () 
+    {
+        if (Rectangulo != null) {
+
+            if ((Bordes.contains(Mapa.getBounds().getNorthEast())) == true && (Bordes.contains(Mapa.getBounds().getSouthWest()) == true)) {
+                UltimoCentro = Mapa.getCenter();
+            }
+            else {
+                Mapa.panTo(UltimoCentro);
+            }
+        }
+    });
+
+    // Validacion de zoom para que no se salga del extremo del rectangulo en caso de que este muy cerca del borde
+	ZoomEscucha = google.maps.event.addListener(Mapa, 'zoom_changed', function (event) 
+    {
+        if (Mapa.getZoom() >= ZoomMinimo) 
+        {
+            ZoomBordes = google.maps.event.addListenerOnce(Mapa, 'bounds_changed', function (event) 
+            {
+                if (Rectangulo != null) 
+                {
+                    if (CondicionZoom == false)
+                        if ((Bordes.contains(Mapa.getBounds().getNorthEast())) == false || (Bordes.contains(Mapa.getBounds().getSouthWest()) == false)) 
+                        {
+                            CondicionZoom = true;
+                            Mapa.fitBounds(Bordes);
+                            CondicionZoom = true;
+                            Mapa.setZoom(ZoomMinimo);
+                        }
+                    CondicionZoom = false;
+                }
+            });
+    	}
+        else 
+        {
+            Mapa.setZoom(ZoomMinimo);
+        }
+    });
+
  }
 
 
@@ -31,7 +65,7 @@ var UltimoCentro = null;
  // Delimita el mapa impidiendo que el usuario se mueva fuera de los limites
  function DelimitarMapa()
  {
- 	  var Rectangulo = new google.maps.Rectangle(
+ 	  Rectangulo = new google.maps.Rectangle(
  	  {
           strokeColor: '#FF0000',
           strokeOpacity: 0.4,
@@ -42,6 +76,8 @@ var UltimoCentro = null;
           bounds: Mapa.getBounds()
       });
 
+ 	  Bordes = Mapa.getBounds();
+ 	  ZoomMinimo = Mapa.getZoom();
  	  UltimoCentro = Mapa.getCenter();
  	  google.maps.event.addListener(Rectangulo, 'rightclick', function(event) 
  	  {
@@ -57,6 +93,7 @@ var UltimoCentro = null;
 
 // Metodos para hacer operaciones de conversiones de coordenada
 
+//Convierte un objeto de latitudlongitud a coordenada x e y
 function LatLngAPunto(latLngPunto) 
 {
   var TopeDerecha = Mapa.getProjection().fromLatLngToPoint(Mapa.getBounds().getNorthEast());
@@ -66,6 +103,7 @@ function LatLngAPunto(latLngPunto)
   return new google.maps.Point((CoordenadaMundo.x - IzquierdaInferior.x) * Escala, (CoordenadaMundo.y - TopeDerecha.y) * Escala);
 }
 
+// Convierte una coordenada x e y a un punto en el mapa
 function PuntoALatLng(Punto) 
 {
   var TopeDerecha = Mapa.getProjection().fromLatLngToPoint(Mapa.getBounds().getNorthEast());
