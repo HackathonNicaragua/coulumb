9d6872aa141d7d12deaf7112a0a173891d2e9141
Oscar <riveramoreira04@gmail.com>
2017-11-25 18:10:11 -0600

    Union Dos versiones archivo EventosNavegador
    Union de los archivos compartidos entre compañeros

--- JSGLOCSE/EventosNavegador.js
+++ JSGLOCSE/EventosNavegador.js
@@ -9,6 +9,3 @@ MarcadoresCollecion.push({Horas:[], X:0, Y:0});
 Proyecciones.push({MarcadoresCollecion: [], Contador:1, CentrosEventuales : new Array(), XDispersion:0, YDispersion:0, XYCorrelacion:0, XCentro:0, YCentro:0, XSigma: 0, YSigma:0, XExactitud:0, YExactitud:0, FhiExactitud:0, PhiExactitud:0, R:0, Angulo:0, FhiSigma2:0, PhiSigma2:0, FhiRadio:0, PhiRadio:0});
 Proyecciones.push({MarcadoresCollecion: [], Contador:1, CentrosEventuales : new Array(), XDispersion:0, YDispersion:0, XYCorrelacion:0, XCentro:0, YCentro:0, XSigma: 0, YSigma:0, XExactitud:0, YExactitud:0, FhiExactitud:0, PhiExactitud:0, R:0, Angulo:0, FhiSigma2:0, PhiSigma2:0, FhiRadio:0, PhiRadio:0});
 Proyecciones.push({MarcadoresCollecion: [], Contador:1, CentrosEventuales : new Array(), XDispersion:0, YDispersion:0, XYCorrelacion:0, XCentro:0, YCentro:0, XSigma: 0, YSigma:0, XExactitud:0, YExactitud:0, FhiExactitud:0, PhiExactitud:0, R:0, Angulo:0, FhiSigma2:0, PhiSigma2:0, FhiRadio:0, PhiRadio:0});
-
-
-}
