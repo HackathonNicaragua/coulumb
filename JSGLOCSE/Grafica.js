01a48ae47a7c9e1a3f7cab57e5a761da951ceb19
Oscar <riveramoreira04@gmail.com>
2017-11-26 01:24:07 -0600

    UFFF Dolor de cabeza, xD Formula para calcular superficie de consumo energetico
    Formula extraida del libro Suministro Electrico de empresas industriales. A.A Feodorov y Eduardo Rodriguez.
    
    Se hace uso de la operaciones matemáticas, de funciones exponenciales.
    
    Concatenacion de Arreglos, uno de uso auxiliar, y Puntos_Z que es donde se guarda la estructura de datos que se utilizarán para la gráfica de la montaña de consumo

--- JSGLOCSE/Grafica.js
+++ JSGLOCSE/Grafica.js
@@ -168,9 +168,12 @@ function Graficando_Hill()
 
 		for (J = LimitY1; J <= LimitY2 - 1; J++)
 		{
-			
+			Auxiliar.push((((1) / (2 * (Math.PI) * (Proyecciones[ProyeccionActiva].XSigma) * (Proyecciones[ProyeccionActiva].YSigma) * (Math.sqrt(1 - (Math.pow(Proyecciones[ProyeccionActiva].R, 2)))))) * (Math.pow((Math.E), (((-1 / (2 * (1 - (Math.pow(Proyecciones[ProyeccionActiva].R, 2))))) * (((Math.pow((I - Proyecciones[ProyeccionActiva].XCentro), 2)) / (Math.pow(Proyecciones[ProyeccionActiva].XSigma, 2))) - (((2 * Proyecciones[ProyeccionActiva].R) * (I - Proyecciones[ProyeccionActiva].XCentro) * (J - Proyecciones[ProyeccionActiva].YCentro)) / (Proyecciones[ProyeccionActiva].XSigma * Proyecciones[ProyeccionActiva].YSigma)) + ((Math.pow((J - Proyecciones[ArreglosGrafica].YCentro), 2)) / (Math.pow(Proyecciones[ProyeccionActiva].YSigma, 2))))))))));
 		}
-
+		
+		Puntos_Z.push(Auxiliar);
 	}
-
 }
+
+
+
