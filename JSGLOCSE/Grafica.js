5305b7e8aa1f6999e04857738c51ee2fabcf785e
Oscar <riveramoreira04@gmail.com>
2017-11-25 22:19:11 -0600

    Datos_Graficas
    Creacion de archivo para graficas de consumidores.. Por el momento los nombres de los canvas es asignado, una vez que sean añadidos a la parte de Grafica en el Index.html se corregirá

---
+++ JSGLOCSE/Grafica.js
@@ -0,0 +1,18 @@
+function Datos_Graficas(Proyecciones, ProyeccionActiva)
+{
+	for (var I = 0; I <= 0; I++) 
+	{
+		//Nombre temporal de canvas, asignacion de nombre, una vez esté terminada la interfaz
+		$("#GraficaA").empty();	
+		$("#GraficaB").empty();	
+		$("#GraficaC").empty();	
+		$("#GraficaD").empty();	
+		$("#GraficaE").empty();	
+		$("#GraficaF").empty();	
+		$("#GraficaG").empty();	
+		$("#GraficaH").empty();	
+		$("#GraficaI").empty();	
+		$("#GraficaJ").empty();	
+		$("#myChartTOTAL").empty();	
+	}
+}
